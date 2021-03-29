6c07e7eef5ddebafb3db50f16e1171aa697c971f
parkaejinn <pp9794@naver.com>
2021-03-29 16:39:07 +0900

    hello-unicorn

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 
 var _config = {
     cognito: {
-        userPoolId: 'ap-northeast-2_bOqTEkLES', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ggcc8ruqjknse4skrsddvt6dr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-2_NJMJj9Mm3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2pic63k4ci68g5lpjiq26u87u7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2', // e.g. us-east-2
     },
     api: {
