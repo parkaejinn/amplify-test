91c841e645b5a977ca3b5fd3779f738d7d735531
parkaejinn <pp9794@naver.com>
2021-03-29 18:50:00 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,21 @@
 
-var _config = {
-    cognito: {
-        userPoolId: 'ap-northeast-2_NJMJj9Mm3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2pic63k4ci68g5lpjiq26u87u7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-2', // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: 'Base URL of your API including the stage', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
-    }
+window._config = {
+
+    시크릿 : {
+
+        userPoolId : 'ap-northeast-2_NJMJj9Mm3', // 예 : us-east-2_uXboG5pAb         
+
+        userPoolClientId : '2pic63k4ci68g5lpjiq26u87u7', // 예 : 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        지역 : 'ap-northeast-2'// 예 : us-east-2 
+
+    }, 
+
+    api : { 
+
+        invokeUrl : 'https://ljhrzqvm2a.execute-api.ap-northeast-2.amazonaws.com/prod'// 예 : https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+    } 
+
 };
     
